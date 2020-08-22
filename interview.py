d3f22b2d72a3c2d6b7e4aafd282f36c4248334a2
yujianzeng <yujianzeng@tencent.com>
2020-08-22 23:13:29 +0800

    update

---
+++ interview.py
@@ -0,0 +1,30 @@
+def distance(str1, str2):
+    m, n = len(str1), len(str2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    if m == 0:
+        return n
+    if n == 0:
+        return m
+    for i in range(1, m + 1):
+        dp[i][0] = i
+    for j in range(1, n + 1):
+        dp[0][j] = j
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if str1[i - 1] == str2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            else:
+                dp[i][j] = min(dp[i - 1][j - 1] + 1, dp[i][j - 1] + 1, dp[i - 1][j] + 1)
+
+    return dp[-1][-1]
+
+
+"""
+
+编辑距离  8.19面试csig 成都
+"""
+str1 = "abcef"
+str2 = "abc"
+res = distance(str1, str2)
+print(res)
